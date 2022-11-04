d6d3612433f5f15349f6abdc2b4b5c1bb49b7ed1
Varga Szabolcs <killerkiegve@gmail.com>
2022-11-04 21:28:55 +0100

    Updated upload.php

--- upload/upload.php
+++ upload/upload.php
@@ -3,49 +3,51 @@
 <html>
 <head>
   <title>Buli App - Esemény feltöltés</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="../css/logreg.css">
 </head>
 <body>
   <div class="header">
   	<h2>Esemény feltöltés</h2>
-  </div>
-	
   <form method="post" action="upload.php">
-  	<?php include('errors.php'); ?>
-  	<div class="input-group">
-  	  <label>Esemény neve</label>
-  	  <input type="text" name="nev" value="<?php echo $nev; ?>">
+  	<?php include('../errors.php'); ?>
+  	<div class="txt_field">
+		<input type="text" name="nev" value="<?php echo $nev; ?>">
+		<span></span>
+		<label>Esemény neve</label>
   	</div>
-  	<div class="input-group">
-  	  <label>Kép</label>
-  	  <input type="text" name="kep" value="<?php echo $kep; ?>">
+  	<div class="txt_field">
+		<input type="text" name="kep" value="<?php echo $kep; ?>">
+		<span></span>
+		<label>Kép</label>
   	</div>
-  	<div class="input-group">
-  	  <label>Időpont</label>
-  	  <input type="date" name="idopont" value="<?php echo $idopont; ?>">
+  	<div class="txt_field">
+		<input type="date" name="idopont" value="<?php echo $idopont; ?>">
+		<span></span>
+		<label>Időpont</label>
   	</div>
-  	  	<div class="input-group">
-  	  <label>Leírás</label>
-  	  <input type="text" name="leiras" value="<?php echo $leiras; ?>">
+  	<div class="txt_field">
+  		<input type="text" name="leiras" value="<?php echo $leiras; ?>">
+		<span></span>
+  		<label>Leírás</label>
   	</div>  	
-  	<div class="input-group">
-  	  <label>Helyszín</label>
-  	  <input type="text" name="helyszin" value="<?php echo $helyszin; ?>">
+  	<div class="txt_field">
+		<input type="text" name="helyszin" value="<?php echo $helyszin; ?>">
+		<span></span>
+		<label>Helyszín</label>
   	</div>  	
-  	<div class="input-group">
-  	  <label>Esemény típusa</label>
-  	  <input type="text" name="event" value="<?php echo $event; ?>">
+  	<div class="txt_field">
+		<input type="text" name="event" value="<?php echo $event; ?>">
+		<span></span>
+		<label>Esemény típusa</label>
   	</div>
-  	<div class="input-group">
-  	  <label>Jegy link</label>
-  	  <input type="text" name="jegylink" value="<?php echo $jegylink; ?>">
+  	<div class="txt_field">
+  	  	<input type="text" name="jegylink" value="<?php echo $jegylink; ?>">
+		<span></span>
+		<label>Jegy link</label>
   	</div>
-  	<div class="input-group">
-  	  <button type="submit" class="btn" name="upload_event">Esemény feltöltése</button>
-  	</div>
-	<div class="input-group">
+  	<button type="submit" class="btn" name="upload_event">Esemény feltöltése</button>
     <button type="submit" name="back"  class="btn">Vissza</button>
-	</div>
   </form>
+</div>
 </body>
 </html>
